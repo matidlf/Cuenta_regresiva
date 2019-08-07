f51f91ecb82ac658525558f1f135ec08bf3a1148
matidlf <50118221+matidlf@users.noreply.github.com>
2019-08-07 00:51:35 -0300

    Add files via upload

---
+++ cuentaregresiva.js
@@ -0,0 +1,29 @@
+﻿function ConteoRegresivo()
+{
+    var fecha=new Date('2019','8','21','00','00','00')
+    var hoy=new Date()
+    var dias=0
+    var horas=0
+    var minutos=0
+    var segundos=0
+    if (fecha>hoy)
+    {
+        var diferencia=(fecha.getTime()-hoy.getTime())/1000 //Resta la fecha definida con la fecha de hoy
+        dias=Math.floor(diferencia/86400) //Devide la diferencia con 86400 segundos que son 3600 x 24 ,lo que da 24 hs
+        diferencia=diferencia-(86400*dias)
+        horas=Math.floor(diferencia/3600)
+        diferencia=diferencia-(3600*horas)
+        minutos=Math.floor(diferencia/60)
+        diferencia=diferencia-(60*minutos)
+        segundos=Math.floor(diferencia)
+        document.getElementById('contador').innerHTML = '' + dias + ' Días' + ' : ' + horas + ' Horas' + ' : ' + minutos + ' Minutos' + ' : ' + segundos + ' Segundos';
+        if (dias>0 || horas>0 || minutos>0 || segundos>0)
+        {
+            setTimeout("ConteoRegresivo()",1000)
+        }
+    }
+    else
+    {
+        document.getElementById('contador').innerHTML = '0 Días ¡Comenzo la PRIMAVERA!';
+    }
+}
